ba85f54d2965448f1bfb85a3b7e2cad3224c9cd6
nhscomputing2021 <81550918+nhscomputing2021@users.noreply.github.com>
2022-03-17 11:52:18 +1100

    Add files via upload

---
+++ u301_prac_03/u301_prac_03/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace u301_prac_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comFilter = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter by: ";
+            // 
+            // comFilter
+            // 
+            this.comFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comFilter.FormattingEnabled = true;
+            this.comFilter.Items.AddRange(new object[] {
+            "Category",
+            "Rating"});
+            this.comFilter.Location = new System.Drawing.Point(411, 52);
+            this.comFilter.Name = "comFilter";
+            this.comFilter.Size = new System.Drawing.Size(144, 37);
+            this.comFilter.TabIndex = 1;
+            this.comFilter.SelectedIndexChanged += new System.EventHandler(this.comFilter_SelectedIndexChanged);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(561, 54);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(188, 35);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(100, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(819, 329);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.comFilter);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comFilter;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ u301_prac_03/u301_prac_03/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace u301_prac_03
+{
+    public partial class Form1 : Form
+    {
+        string filter;
+        List<Sale> sales = new List<Sale>();
+        BindingSource bs = new BindingSource();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCSV();
+            bs.DataSource = sales;
+            dataGridView1.DataSource = bs;
+        }
+
+        private void LoadCSV()
+        {
+            string filePath = @"C:\demo\sample data_1.csv";
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList();
+            foreach (string line in lines)
+            {
+                List<string> fields = line.Split(',').ToList();
+                Sale s = new Sale();
+                s.ItemName = fields[0];
+                s.Category = fields[1];
+                s.PurchasedPrice = float.Parse(fields[3]);
+                s.SalePrice = fields[5];
+                s.Rating = fields[6];
+                sales.Add(s);
+            }
+        }
+
+        private void comFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filter = comFilter.Text;
+            if (filter == "Rating") SortByRating(sales);
+            dataGridView1.DataSource = bs;
+            bs.ResetBindings(false);
+        }
+        private void SortByRating(List<Sale> _sales)
+        {//adapted version of selection sort
+            int  min;
+            string temp;
+            for (int i = 0; i < _sales.Count - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < _sales.Count; j++)
+                {
+                    
+                    if(int.TryParse(_sales[j].Rating, out int ratingJ))
+                    { /*
+                        if the current rating is a number and the current smallest 
+                        is also a number and is greater than the current rating,
+                        then assign the index of current rating to the index of the smallest
+                        else if the smallest is NA, which is greater than the number, 
+                        so assign the index of current rating to the index of the smallest
+
+                      */
+                       
+                        if(int.TryParse(_sales[min].Rating, out int ratingMin))
+                            { if (ratingJ < ratingMin) min = j; }
+                        else
+                        {
+                            min = j;
+                        }
+                    }
+                }
+                //swap the current element with the current smallest one
+                temp = _sales[min].Rating;
+                _sales[min].Rating = _sales[i].Rating;
+                _sales[i].Rating = temp;
+            }
+        }
+
+        private List<Sale> Search(string target,string filter)
+        {
+            List<Sale> results = new List<Sale>();
+            foreach(Sale s in sales)
+            {
+                if(filter == "Rating")
+                {
+                    if (s.Rating.ToLower() == target.ToLower()) results.Add(s);
+                }
+                if(filter == "Category")
+                {
+                    if (s.Category.ToLower().Contains(target.ToLower())) { results.Add(s); Console.WriteLine(target); }
+                }
+            }
+            return results;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            List<Sale> r = Search(txtSearch.Text, filter);
+            bs.DataSource = r;
+            dataGridView1.DataSource = r;
+            bs.ResetBindings(false);
+        }
+    }
+}
